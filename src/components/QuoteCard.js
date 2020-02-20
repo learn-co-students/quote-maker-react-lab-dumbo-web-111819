8bcb83a6677d5d79e29bc121bf616b0b3fe3480a
Valentine <valentinemaillard1@gmail.com>
2020-02-19 22:38:21 -0500

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,61 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
+
+const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) => {
+  
+  // let {content, author, votes, id} = props.quote
+  
+  const deleteQuote = () => {
+    removeQuote(quote.id)
+  }
+
+  const handleUpVote = () => {
+    upvoteQuote(quote.id)
+  }
+
+  const handleDownVote = () => {
+    downvoteQuote(quote.id)
+  }
+
+  return (
+
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{quote.content}</p>
+          <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
           <button
+            onClick={handleUpVote}
             type="button"
             className="btn btn-primary"
           >
             Upvote
           </button>
           <button
+            onClick={handleDownVote}
             type="button"
             className="btn btn-secondary"
           >
             Downvote
           </button>
           <button
+            onClick={deleteQuote}
             type="button"
             className="btn btn-danger"
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {quote.votes}</div>
       </div>
     </div>
-  </div>;
+  </div>
+  )
+}
 
 export default QuoteCard;
